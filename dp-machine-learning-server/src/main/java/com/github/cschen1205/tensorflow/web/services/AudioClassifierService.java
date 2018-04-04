97204c1feb59f154e04ac1532fe91c32aa7539b6
xschen <cschen1205@gmail.com>
2018-04-04 11:22:37 +0800

    fix the configuration issue

--- dp-machine-learning-server/src/main/java/com/github/cschen1205/tensorflow/web/services/AudioClassifierService.java
+++
@@ -1,8 +0,0 @@
-package com.github.cschen1205.tensorflow.web.services;
-
-import java.io.File;
-
-public interface AudioClassifierService extends TrainedModelService {
-    String predict(File audioFile);
-
-}
